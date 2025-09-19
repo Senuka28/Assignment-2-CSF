5b8440712e73758f7edb245e7582ebc4e20afeaf
Senuka Abeysinghe <senukaabeysinghe@Senukas-MacBook-Pro.local>
2025-09-19 02:23:28 -0400

    fixed complement function

--- csf_assign02/c_imgproc_fns.c
+++ csf_assign02/c_imgproc_fns.c
@@ -23,9 +23,9 @@ void imgproc_complement( struct Image *input_img, struct Image *output_img ) {
   for (int i = 0; i < size; i++) {
     uint32_t pixel_data = input_img->data[i];
 
-    uint32_t alpha_save = pixel_data & 0xFF000000; // only stores alpha bits
-    uint32_t rgb_save = pixel_data & 0x00FFFFFF; // only stores the rbg bits
-    uint32_t rgb_flip = (~rgb_save) & 0x00FFFFFF; // flip the rgb bits
+    uint32_t alpha_save = pixel_data & 0x000000FF; // only stores alpha bits
+    uint32_t rgb_save = pixel_data & 0xFFFFFF00; // only stores the rbg bits
+    uint32_t rgb_flip = (~rgb_save) & 0xFFFFFF00; // flip the rgb bits
 
     output_img->data[i] = alpha_save | rgb_flip; 
   }
